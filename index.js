0f76c3c9ffec40d60242f7d646ba6cb9067e9e2e
Elaine Thai <ethai1827@gmail.com>
2019-05-14 22:51:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- cart.push({item: item, itemPrice: Math.floor(Math.random() * 100) + 1})
+ cart.push({itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1})
 }
 
 function viewCart() {
