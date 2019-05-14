3b9579e4d457e8627f21887a2d2946f5baf58f2e
Elaine Thai <ethai1827@gmail.com>
2019-05-14 23:33:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,9 +52,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber !== undefined) {
-    var total = total();
+    var t = total();
     cart = [];
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${t}, which will be charged to the card ${cardNumber}.`
   } else {
     return "Sorry, we don't have a credit card on file for you."
   }
