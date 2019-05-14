e6f0efcdac5174676e0c6fea6f135fd3899b8a14
Elaine Thai <ethai1827@gmail.com>
2019-05-14 23:32:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function total() {
 
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
-    if (cart[i][itemName] === item) {
+    if (cart[i].itemName === item) {
       cart.splice(i, 1);
       return cart;
     }
