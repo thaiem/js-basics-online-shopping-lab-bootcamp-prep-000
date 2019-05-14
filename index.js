1f4fe8db5e3fe712f352285cbf9f7edc8d9dfc29
Elaine Thai <ethai1827@gmail.com>
2019-05-14 22:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,10 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    total += cart[i][itemPrice]
+  }
 }
 
 function removeFromCart(item) {
