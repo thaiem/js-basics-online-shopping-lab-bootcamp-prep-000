ef3fbed4e46980229269141b62751c109e5eff4d
Elaine Thai <ethai1827@gmail.com>
2019-05-14 23:04:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,5 +49,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber !==)
+  if (cardNumber !== undefined) {
+    
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
